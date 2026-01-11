6e11337554eacf1cfb20a53710c03943e898a83c
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2026-01-11 22:54:28 +0100

    Add Like model with properties and methods for like management

---
+++ app/Models/Like.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Models;   
+class Like {
+    private $id;
+    private $created_at;
+
+    public function __construct($id, $created_at) {
+        $this->id = $id;
+        $this->created_at = date("Y-m-d H:i:s");
+    }
+    public function getId() {
+        return $this->id;
+    }
+    public function getCreatedAt() {
+        return $this->created_at;
+    }
+}
