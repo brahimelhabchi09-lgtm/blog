bf3519cf9699f6dcc7f43fbe61ce6b428c71e3ea
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2026-01-11 22:55:53 +0100

    Fix namespace import for Router class and ensure routes are loaded

--- public/index.php
+++ public/index.php
@@ -3,10 +3,12 @@
 
 require_once __DIR__ . '/../bootstrap/autoload.php';
 
-use App\Core\Router;
+use Core\Router\Router;
 
 session_start();
 
 $router = new Router();
+
+require_once __DIR__ . '/../routes/web.php';
 
 $router->dispatch();
